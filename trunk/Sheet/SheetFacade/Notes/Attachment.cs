b886268e48848412d9220748ee997c72d2e6cfe6
m.vin@hotmail.com <m.vin@hotmail.com@4f5f8896-3787-d572-7464-05abdb9f40d3>
2013-11-24 17:07:52 +0000

    Added facade interfaces.

---
+++ trunk/Sheet/SheetFacade/Notes/Attachment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SheetFacade.Notes
+{
+    interface Attachment
+    {
+        string Name { get; set; }
+        string Path { get; set; }
+        string MimeType { get; set; }
+        IEnumerable<Metainfo> Metadata { get; }
+    }
+}
---
+++ trunk/Sheet/SheetFacade/Notes/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SheetFacade.Notes
+{
+    interface Note
+    {
+        string Title { get; set; }
+        DateTime DateOfCreation { get; set; }
+        DateTime LastModified { get; set; }
+        IList<Label> Labels { get; }
+    }
+}
