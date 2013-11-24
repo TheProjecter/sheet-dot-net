4b4e9b75acd802da1d9a8312ddddd90c67a79268
m.vin <m.vin@hotmail.com>
2013-11-24 17:07:52 +0000

    Added facade interfaces.

---
+++ Sheet/SheetFacade/Notes/Metainfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SheetFacade.Notes
+{
+    interface Metainfo
+    {
+        string Key { get; set; }
+        string Value { get; set; }
+    }
+}
