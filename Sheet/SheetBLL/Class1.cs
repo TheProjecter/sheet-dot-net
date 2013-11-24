8dc7e98017ebd128186fcea5b6071e8f4e251e98
m.vin <m.vin@hotmail.com>
2013-11-24 12:59:18 +0000

    Project structure set up. SheetDAL: Data Access Layer, provides the Entity Framework implementation of DB management. SheetBLL: Business Logic Layer, provides high-level mechanisms to create and manipulate data using the DAL. SheetGUI: Graphical User Interface, provides the user with an interface to conveniently access function of the BLL. SheetFacade: Provides the common interfaces used to exchange data between layers implemented in the DAL, BLL and GUI. Can also be used to decouple layers and distribute them in a network environment.

---
+++ Sheet/SheetBLL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SheetBLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sheet/SheetDAL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SheetDAL
+{
+    public class Class1
+    {
+    }
+}
