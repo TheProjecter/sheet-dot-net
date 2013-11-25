683d100049e9363782babecc613f2aff05dcbb7d
segesdi.dani@gmail.com <segesdi.dani@gmail.com@4f5f8896-3787-d572-7464-05abdb9f40d3>
2013-11-25 15:21:22 +0000

    Added connection string to App.config

---
+++ trunk/Sheet/SheetDAL/SheetContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sheet.DAL.Entities;
+
+namespace Sheet.DAL
+{
+    class SheetContext : DbContext
+    {
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Label> Lables { get; set; }
+        public DbSet<Metainfo> Metadata { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        public SheetContext(string connStr) : base(connStr)
+        {
+            
+        }
+
+        public SheetContext() : base()
+        {
+
+        }
+    }
+}
