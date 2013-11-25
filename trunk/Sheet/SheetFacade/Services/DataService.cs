a0775cd14cd0bf117c536dfd70050c38b083d3db
m.vin@hotmail.com <m.vin@hotmail.com@4f5f8896-3787-d572-7464-05abdb9f40d3>
2013-11-25 16:29:04 +0000

    Query framework sketch.

---
+++ trunk/Sheet/SheetFacade/Services/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sheet.Facade.Notes;
+using Sheet.Facade.Queries;
+
+namespace Sheet.Facade.Services
+{
+    public interface DataService
+    {
+        Note CreateNote();
+        Attachment CreateAttachment();
+        Label CreateLabel();
+        Metainfo CreateMetainfo();
+
+        bool SaveNote(Note note);
+
+        bool DeleteNote(Note note);
+
+        Note RefreshNote(Note note);
+        ICollection<Note> QueryNotes(Label label);
+        ICollection<Note> QueryNotes(NoteQuery query);
+    }
+}
