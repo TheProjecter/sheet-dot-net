6303c63fd5db036e16ce6cf84b1318d5c3339176
m.vin@hotmail.com <m.vin@hotmail.com@4f5f8896-3787-d572-7464-05abdb9f40d3>
2013-11-30 14:51:15 +0000

    Started to refactor viewmodels into a factory.

---
+++ trunk/Sheet/SheetGUI/ViewModel/ViewModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sheet.GUI.ViewModel
+{
+    public class ViewModelFactory
+    {
+        private IDictionary<int, LabelViewModel> labelViewModels = new Dictionary<int, LabelViewModel>();
+        private IDictionary<int, NoteViewModel> noteViewModels = new Dictionary<int, NoteViewModel>();
+        private IDictionary<int, AttachmentViewModel> attachmentViewModels = new Dictionary<int, AttachmentViewModel>();
+        private IDictionary<int, MetainfoViewModel> metainfoViewModels = new Dictionary<int, MetainfoViewModel>();
+
+        //TODO: add methods to create viewmodels for models, storing the id to retrieve any existing viewmodel if already created.
+    }
+}
